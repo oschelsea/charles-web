904a720b6e292e522599e193d818519791ab5c49
chelsea <chel_sea@126.com>
2026-02-04 17:04:21 +0800

    添加soybean vue3前端

---
+++ manager-ui-vue3/src/typings/api/route.d.ts
@@ -0,0 +1,23 @@
+declare namespace Api {
+  /**
+   * namespace Route
+   *
+   * backend api module: "route"
+   */
+  namespace Route {
+    type ElegantConstRoute = import('@elegant-router/types').ElegantConstRoute;
+
+    interface MenuRoute extends ElegantConstRoute {
+      id: string;
+    }
+
+    interface UserRoute {
+      routes: MenuRoute[];
+      home: import('@elegant-router/types').LastLevelRouteKey;
+    }
+
+    interface RoutesData {
+      data: ElegantConstRoute[]
+    }
+  }
+}
